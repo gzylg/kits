bd47fbc5bd36a6a30d17386b35689106b1759db4
gzylg <5560636@qq.com>
2022-04-23 16:09:47 +0800

    update

--- crypt/aes/aes.go
+++ crypt/aes/aes.go
@@ -10,55 +10,60 @@ import (
 )
 
 // =================== CBC ======================
-func EncryptCBCBase64(origData, key string) (err error, encrypted string) {
+func EncryptCBCBase64(origData, key string) (encrypted string, err error) {
 	var b []byte
-	if err, b = EncryptCBC([]byte(origData), []byte(key)); err != nil {
-		return err, ""
+	if b, err = EncryptCBC([]byte(origData), []byte(key)); err != nil {
+		return
 	}
-	return nil, base64.StdEncoding.EncodeToString(b)
+	return base64.StdEncoding.EncodeToString(b), nil
 }
-func DecryptCBCFromBase64(encrypted string, key string) (err error, decrypted string) {
+
+func DecryptCBCFromBase64(encrypted string, key string) (decrypted string, err error) {
 	var b []byte
 	if b, err = base64.StdEncoding.DecodeString(encrypted); err != nil {
-		return err, ""
+		return
 	}
 
-	if err, b = DecryptCBC(b, []byte(key)); err != nil {
-		return err, ""
+	if b, err = DecryptCBC(b, []byte(key)); err != nil {
+		return
 	}
-	return nil, string(b)
+	return string(b), nil
 }
-func EncryptCBC(origData, key []byte) (err error, encrypted []byte) {
+
+func EncryptCBC(origData, key []byte) (encrypted []byte, err error) {
 	// 分组秘钥
 	// NewCipher该函数限制了输入k的长度必须为16, 24或者32
 	block, err := aes.NewCipher(key)
 	if err != nil {
-		return err, nil
+		return nil, err
 	}
 	blockSize := block.BlockSize()                              // 获取秘钥块的长度
 	origData = pkcs5Padding(origData, blockSize)                // 补全码
 	blockMode := cipher.NewCBCEncrypter(block, key[:blockSize]) // 加密模式
 	encrypted = make([]byte, len(origData))                     // 创建数组
 	blockMode.CryptBlocks(encrypted, origData)                  // 加密
-	return nil, encrypted
+	return encrypted, nil
 }
-func DecryptCBC(encrypted, key []byte) (err error, decrypted []byte) {
+
+func DecryptCBC(encrypted, key []byte) (decrypted []byte, err error) {
 	block, err := aes.NewCipher(key) // 分组秘钥
 	if err != nil {
-		return err, nil
+		return nil, err
 	}
 	blockSize := block.BlockSize()                              // 获取秘钥块的长度
 	blockMode := cipher.NewCBCDecrypter(block, key[:blockSize]) // 加密模式
 	decrypted = make([]byte, len(encrypted))                    // 创建数组
 	blockMode.CryptBlocks(decrypted, encrypted)                 // 解密
 	decrypted = pkcs5UnPadding(decrypted)                       // 去除补全码
-	return nil, decrypted
+	return decrypted, nil
 }
+
 func pkcs5Padding(ciphertext []byte, blockSize int) []byte {
 	padding := blockSize - len(ciphertext)%blockSize
 	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
 	return append(ciphertext, padtext...)
 }
+
 func pkcs5UnPadding(origData []byte) []byte {
 	length := len(origData)
 	unpadding := int(origData[length-1])
@@ -66,27 +71,29 @@ func pkcs5UnPadding(origData []byte) []byte {
 }
 
 // =================== ECB(速度应该是最快的) ======================
-func EncryptECBBase64(origData, key string) (err error, encrypted string) {
+func EncryptECBBase64(origData, key string) (encrypted string, err error) {
 	var b []byte
-	if err, b = EncryptECB([]byte(origData), []byte(key)); err != nil {
-		return err, ""
+	if b, err = EncryptECB([]byte(origData), []byte(key)); err != nil {
+		return
 	}
-	return nil, base64.StdEncoding.EncodeToString(b)
+	return base64.StdEncoding.EncodeToString(b), nil
 }
-func DecryptECBFromBase64(encrypted string, key string) (err error, decrypted string) {
+
+func DecryptECBFromBase64(encrypted string, key string) (decrypted string, err error) {
 	var b []byte
 	if b, err = base64.StdEncoding.DecodeString(encrypted); err != nil {
-		return err, ""
+		return
 	}
-	if err, b = DecryptECB(b, []byte(key)); err != nil {
-		return err, ""
+	if b, err = DecryptECB(b, []byte(key)); err != nil {
+		return
 	}
-	return nil, string(b)
+	return string(b), nil
 }
-func EncryptECB(origData, key []byte) (err error, encrypted []byte) {
+
+func EncryptECB(origData, key []byte) (encrypted []byte, err error) {
 	cipher, err := aes.NewCipher(generateKey(key))
 	if err != nil {
-		return err, nil
+		return nil, err
 	}
 	length := (len(origData) + aes.BlockSize) / aes.BlockSize
 	plain := make([]byte, length*aes.BlockSize)
@@ -101,12 +108,13 @@ func EncryptECB(origData, key []byte) (err error, encrypted []byte) {
 		cipher.Encrypt(encrypted[bs:be], plain[bs:be])
 	}
 
-	return nil, encrypted
+	return encrypted, nil
 }
-func DecryptECB(encrypted, key []byte) (err error, decrypted []byte) {
+
+func DecryptECB(encrypted, key []byte) (decrypted []byte, err error) {
 	cipher, err := aes.NewCipher(generateKey(key))
 	if err != nil {
-		return err, nil
+		return nil, err
 	}
 	decrypted = make([]byte, len(encrypted))
 	//
@@ -119,8 +127,9 @@ func DecryptECB(encrypted, key []byte) (err error, decrypted []byte) {
 		trim = len(decrypted) - int(decrypted[len(decrypted)-1])
 	}
 
-	return nil, decrypted[:trim]
+	return decrypted[:trim], nil
 }
+
 func generateKey(key []byte) (genKey []byte) {
 	genKey = make([]byte, 16)
 	copy(genKey, key)
@@ -133,35 +142,36 @@ func generateKey(key []byte) (genKey []byte) {
 }
 
 // =================== CFB（速度最慢的） ======================
-func EncryptCFB(origData, key []byte) (err error, encrypted []byte) {
+func EncryptCFB(origData, key []byte) ([]byte, error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
-		return err, nil
+		return nil, err
 	}
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	encrypted = make([]byte, aes.BlockSize+len(origData))
+	encrypted := make([]byte, aes.BlockSize+len(origData))
 	iv := encrypted[:aes.BlockSize]
 	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
-		panic(err)
+		return nil, err
 	}
 	stream := cipher.NewCFBEncrypter(block, iv)
 	stream.XORKeyStream(encrypted[aes.BlockSize:], origData)
-	return nil, encrypted
+	return encrypted, nil
 }
-func DecryptCFB(encrypted, key []byte) (err error, decrypted []byte) {
+
+func DecryptCFB(encrypted, key []byte) ([]byte, error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
-		return err, nil
+		return nil, err
 	}
 	if len(encrypted) < aes.BlockSize {
-		panic("ciphertext too short")
+		return nil, err
 	}
 	iv := encrypted[:aes.BlockSize]
 	encrypted = encrypted[aes.BlockSize:]
 
 	stream := cipher.NewCFBDecrypter(block, iv)
 	stream.XORKeyStream(encrypted, encrypted)
-	return nil, encrypted
+	return encrypted, nil
 }
