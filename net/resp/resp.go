3e58d0543a4bf30bda7c36224447c0b587492339
gzylg <5560636@qq.com>
2022-05-01 04:29:36 +0800

    update

--- net/resp/resp.go
+++ net/resp/resp.go
@@ -1,9 +1,9 @@
 package resp
 
 type Content struct {
-	Code    int         `json:"code"`
-	Result  bool        `json:"result"`
-	Message string      `json:"message"`
-	Data    interface{} `json:"data,omitempty"`
-	Total   *int64      `json:"total,omitempty"`
+	Code    int    `json:"code"`
+	Result  bool   `json:"result"`
+	Message string `json:"message"`
+	Data    any    `json:"data,omitempty"`
+	Total   *int64 `json:"total,omitempty"`
 }
