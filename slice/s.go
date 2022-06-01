26bb50135fb9bee6721ad06292f11afd4d053876
gzylg <5560636@qq.com>
2022-06-01 14:33:10 +0800

    add: slice

---
+++ slice/s.go
@@ -0,0 +1,28 @@
+package slice
+
+// HasString 检查传入的字符串是否存在于slice中
+func HasString(v string, sl []string) bool {
+	if len(sl) < 1 {
+		return false
+	}
+
+	for _, vv := range sl {
+		if vv == v {
+			return true
+		}
+	}
+	return false
+}
+
+func HasInt(i int, il []int) bool {
+	if len(il) < 1 {
+		return false
+	}
+
+	for _, ii := range il {
+		if ii == i {
+			return true
+		}
+	}
+	return false
+}
