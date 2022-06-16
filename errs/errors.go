672ebc63380982b85ea5215da4c9e7a9ed2748e4
gzylg <5560636@qq.com>
2022-06-16 22:03:47 +0800

    update: IsYsErrWithErrType

--- errs/errors.go
+++ errs/errors.go
@@ -48,7 +48,13 @@ func IsYsErr(e1 error, e2 error) bool {
 	return e1.Error() == e2.Error()
 }
 
-// ISYsErrWithStrict 间隔判断两个错误内容是否相同
+// IsYsErr 判断错误内容是否相同，判断e1的msg，是否为传入的errType msg相同
+func IsYsErrWithErrType(e1 error, errType ErrType) bool {
+	msg := GetErrMessage(errType)
+	return msg == e1.Error()
+}
+
+// ISYsErrWithStrict 判断两个错误内容是否相同
 // 先通过 AsYsErr 检查，通过后判断两个错误内容的code和msg是否相同
 func IsYsErrWithStrict(e1 error, e2 error) bool {
 	if !AsYsErr(e1) || !AsYsErr(e2) {
