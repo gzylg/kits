41c2ffaadb9eabdcc639c202fd9a11ad72aa12c6
Mirage <5560636@qq.com>
2023-11-08 02:58:43 +0800

    del

--- slice/s.go
+++ slice/s.go
@@ -11,46 +11,6 @@ func FindIndex[T comparable](slice []T, val T) int {
 	return -1
 }
 
-// HasString 检查传入的字符串是否存在于slice中
-func HasString(v string, sl []string) bool {
-	if len(sl) < 1 {
-		return false
-	}
-
-	for _, vv := range sl {
-		if vv == v {
-			return true
-		}
-	}
-	return false
-}
-
-func HasInt(i int, il []int) bool {
-	if len(il) < 1 {
-		return false
-	}
-
-	for _, ii := range il {
-		if ii == i {
-			return true
-		}
-	}
-	return false
-}
-
-func HasUint32(i uint32, il []uint32) bool {
-	if len(il) < 1 {
-		return false
-	}
-
-	for _, ii := range il {
-		if ii == i {
-			return true
-		}
-	}
-	return false
-}
-
 func ShuffleInt(arr []int) []int {
 	nums := make([]int, len(arr))
 	buf := make([]int, len(arr))
