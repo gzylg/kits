dc798576791d8f637424255f82b571fd44049617
Mirage <5560636@qq.com>
2024-05-13 12:03:39 +0800

    update: des

--- crypt/des/des_test.go
+++ crypt/des/des_test.go
@@ -1,24 +1,27 @@
 package des
 
 import (
+	"encoding/base64"
 	"log"
 	"testing"
-
-	"github.com/gzylg/kits/random"
 )
 
 var (
-	key     = random.Str(8)
-	content = random.Str(32)
+	key    = "Skzd8+wlXNI+dj5lJG7rvT9U" // random.Str(8)
+	ivbyte = []byte("azb_2019")
+
+	//content = random.Str(32)
+	content = `{"UID":"hy_57178310980076","timeSample":1700933329101,"token":"5457F61452610C95B836B65EB78B4D14E6BB5F036360847C"}`
 )
 
 func TestCBC(t *testing.T) {
 	log.Println("content:", content)
 
-	a, err := CBCEncrypt([]byte(content), []byte(key))
+	a, err := CBCEncrypt([]byte(content), []byte(key), ivbyte...)
 	log.Println("Encrypt", a, err)
+	log.Println("Encrypt", base64.StdEncoding.EncodeToString(a))
 
-	b, err := CBCDecrypt(a, []byte(key))
+	b, err := CBCDecrypt(a, []byte(key), ivbyte...)
 	log.Println("Decrypt", string(b), err)
 
 	if string(b) != content {
