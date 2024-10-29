6dcbfbfe25b963453216d104c122b639522bce77
Mirage <5560636@qq.com>
2024-10-29 20:03:40 +0800

    update

--- slice/s.go
+++ slice/s.go
@@ -1,7 +1,11 @@
 package slice
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
 
+// FindIndex 在切片中查找指定值的索引，如果找到则返回索引，否则返回 -1
 func FindIndex[T comparable](slice []T, val T) int {
 	for idx, v := range slice {
 		if v == val {
@@ -11,26 +15,13 @@ func FindIndex[T comparable](slice []T, val T) int {
 	return -1
 }
 
-func ShuffleInt(arr []int) []int {
-	nums := make([]int, len(arr))
-	buf := make([]int, len(arr))
-	copy(buf, arr)
-	for i := range nums {
-		j := rand.Intn(len(buf))
-		nums[i] = buf[j]
-		buf = append(buf[0:j], buf[j+1:]...)
+// Shuffle 随机打乱切片的排列顺序，函数支持任何类型的切片
+func Shuffle[T any](arr []T) []T {
+	r := rand.New(rand.NewSource(time.Now().UnixNano())) // 初始化随机数生成器
+	n := len(arr)
+	for i := n - 1; i > 0; i-- {
+		j := r.Intn(i + 1)              // 生成一个 0 到 i 的随机索引
+		arr[i], arr[j] = arr[j], arr[i] // 交换元素
 	}
-	return nums
-}
-
-func ShuffleUint32(arr []uint32) []uint32 {
-	nums := make([]uint32, len(arr))
-	buf := make([]uint32, len(arr))
-	copy(buf, arr)
-	for i := range nums {
-		j := rand.Intn(len(buf))
-		nums[i] = buf[j]
-		buf = append(buf[0:j], buf[j+1:]...)
-	}
-	return nums
+	return arr
 }
